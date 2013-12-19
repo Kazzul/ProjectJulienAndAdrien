30f836598a8fa270222d33b6a5cff9e7ffeadbc1
Juju92i <parmentier.jul@gmail.com>
2013-12-19 15:29:37 +0100

    Interim App Beginning

---
+++ InterimApplication/InterimApplication/ControlerOne.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using ControlerPat;
+
+namespace InterimApplication
+{
+
+    public class ControlerEntreprisesClientes : ControlerPattern
+    {
+        public override 
+    }
+
+    public class ControlerEmploye : ControlerPattern
+    {
+
+    }
+}
